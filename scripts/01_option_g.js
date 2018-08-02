d3da9fa0e566d6b989c1d538a8435e466b9bc02b
keitango <godspeed4246@gmail.com>
2018-08-02 22:33:11 +0900

    delete files

--- scripts/01_option_g.js
+++
@@ -1,8 +0,0 @@
-// g : 最後まで検索する
-// global
-var text = "114514";
-console.log("text : " + text);
-
-// replace(pat1, pat2) : pat1をpat2に置換する
-console.log("text.replace(/1/,'e') : " + text.replace(/1/, "e"));
-console.log("text.replace(/1/g,'e') : " + text.replace(/1/g, "e"));
--- scripts/02_option_i.js
+++
@@ -1,7 +0,0 @@
-// i : 大文字小文字を区別しない
-var text = "Aa";
-console.log("text : " + text);
-
-// replace(pat1, pat2) : pat1をpat2に置換する
-console.log("text.replace(/a/,'z') : " + text.replace(/a/, "z"));
-console.log("text.replace(/a/i,'z') : " + text.replace(/a/i, "z"));
